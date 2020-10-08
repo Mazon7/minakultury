e1852886287c82bdeed93f2708ad7afe122a6202
Max <maximgusho7@gmail.com>
2020-10-08 23:06:37 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$("#carouselExampleFade").on("slide.bs.carousel", function () {
+  console.log("new");
+});
+
+// $(function () {
+//   $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+// });
